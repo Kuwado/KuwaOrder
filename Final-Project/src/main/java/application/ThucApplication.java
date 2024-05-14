8a083e5eb6c51dd21383ead848f819b1bb630232
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-05-14 23:22:36 +0700

    thuc application

---
+++ Final-Project/src/main/java/application/ThucApplication.java
@@ -0,0 +1,32 @@
+package application;
+
+import fx.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ThucApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        MainController mc = new MainController();
+        mc.setSidebarPath("/view/parts/sidebar/OrderPlacement.fxml");
+        mc.setContentPath("/view/content/site/HandleSiteOrderForm.fxml");
+        mc.setAvatarPath("/images/thuc.jpg");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Main.fxml"));
+        fxmlLoader.setController(mc);
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/styles.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/makeOrder.css")).toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
