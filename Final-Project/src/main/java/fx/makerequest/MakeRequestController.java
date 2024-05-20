8c510006a3049a61454058941a1622626aa9609e
QV142 <cuanne142@gmail.com>
2024-05-21 00:45:15 +0700

    21/05

--- Final-Project/src/main/java/fx/makerequest/MakeRequestController.java
+++ Final-Project/src/main/java/fx/makerequest/MakeRequestController.java
@@ -6,14 +6,25 @@ import fx.makeorder.MOController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.tabledata.MROrder;
 import model.Product;
 import model.Order;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MakeRequestController extends MOController<MROrder> {
 
@@ -57,6 +68,9 @@ public class MakeRequestController extends MOController<MROrder> {
     private TableColumn<MROrder, Integer> quantity;
 
     @FXML
+    private Button chooseProductBtn;
+
+    @FXML
     private Button makeOrderBtn;
 
     @FXML
@@ -64,6 +78,11 @@ public class MakeRequestController extends MOController<MROrder> {
 
     @FXML
     private TableColumn<MROrder, String> unit;
+
+    @FXML
+    void chooseProduct(ActionEvent event) throws IOException {
+        runPopUp("view/popUp/ChooseProduct.fxml", 1250, 700);
+    }
 
     private final ObservableList<MROrder> mrOrders = FXCollections.observableArrayList();
     private final ProductController productController = new ProductController();
@@ -96,8 +115,6 @@ public class MakeRequestController extends MOController<MROrder> {
         table.setItems(mrOrders);
     }
 
-
-
     @Override
     public void setDataToTrans(MROrder confirmSite) {
 
@@ -113,4 +130,32 @@ public class MakeRequestController extends MOController<MROrder> {
         orderExpiredDate.setText(order.getDesiredDate());
     }
 
+    private void runPopUp(String path, double width, double height) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/popUp/ChooseProduct.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initModality(Modality.APPLICATION_MODAL);
+
+        HBox pane = loader.load();
+
+        // Lấy kích thước của màn hình
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
+
+        // Tính toán vị trí để pop-up được hiển thị chính giữa màn hình
+        double popupX = (screenWidth - width) / 2;
+        double popupY = (screenHeight - height) / 2;
+
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/styles.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/makeOrder.css")).toExternalForm());
+        primaryStage.setScene(scene);
+
+        // Đặt vị trí cho pop-up
+        primaryStage.setX(popupX);
+        primaryStage.setY(popupY);
+
+        primaryStage.show();
+    };
 }
